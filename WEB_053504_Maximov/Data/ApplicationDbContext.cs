5b283be2a747394aed6783aa6715f9862c59e111
matuamod <matua.models2003@gmail.com>
2022-09-03 18:13:36 +0300

    Added first

---
+++ WEB_053504_Maximov/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WEB_053504_Maximov.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ WEB_053504_Maximov/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WEB_053504_Maximov.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
